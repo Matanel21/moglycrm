cb4a1f094300abb2f2e2da321e959d74d54dab13
Dream Air <dreamair.store@gmail.com>
2026-03-22 17:49:27 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Contacts from '@/pages/Contacts';
 import RivhitSettings from '@/pages/RivhitSettings';
 import RivhitTest from '@/pages/RivhitTest';
 import SyncDashboard from '@/pages/SyncDashboard';
+import Suppliers from '@/pages/Suppliers';
 import CustomerProfile from '@/pages/CustomerProfile';
 
 const AuthenticatedApp = () => {
